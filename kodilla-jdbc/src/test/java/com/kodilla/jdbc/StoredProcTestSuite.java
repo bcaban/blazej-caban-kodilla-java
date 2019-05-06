489758c92f761004a873e5d0dcd45b00dd7acfbe
bcaban <b.caban1996@gmail.com>
2019-05-06 21:55:01 +0200

    Task 30.3 Call UpdateBestsellers procedure from Java

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -29,4 +29,24 @@ public class StoredProcTestSuite {
         }
         assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY_BESTSELLERS FROM BOOKS WHERE BESTSELLER=TRUE";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howManyBestsellers = -1;
+        if (rs.next()) {
+            howManyBestsellers = rs.getInt("HOW_MANY_BESTSELLERS");
+        }
+        assertEquals(4, howManyBestsellers);
+    }
 }
